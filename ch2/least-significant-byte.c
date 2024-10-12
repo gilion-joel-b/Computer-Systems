577230b3e6d0518aaa28af2292bc1fd687571f20
Joel W. Blomster <joel.blomster@gilion.com>
2024-10-12 10:42:24 +0200

    Bit operations for least significant byte

---
+++ ch2/least-significant-byte.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int val = 0x00000021;
+    int least_significant_byte = val & 0xFF;
+    printf("%d\n", least_significant_byte);
+
+    int all_but_least_significant_byte = val & ~0xFF;
+    printf("%d\n", all_but_least_significant_byte);
+
+    int least_significant_byte_is_only_ones_other_bytes_unchanged = (val & ~0xFF) | 0xFF;
+    printf("%d\n", least_significant_byte_is_only_ones_other_bytes_unchanged);
+
+}
